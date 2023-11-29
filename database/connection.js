912a61c66cc5e518b2b1796af892f9b805f784e9
muqtadir <tadir.muqtadir@gmail.com>
2023-11-29 12:02:48 +0800

    first push

---
+++ database/connection.js
@@ -0,0 +1,34 @@
+import mariadb from 'mariadb'
+import {user, cart} from '../model/user.model.js'
+import 'dotenv/config'
+
+
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST, 
+    user:process.env.DB_USER, 
+    database:process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port:process.env.DB_PORT
+});
+
+export const dbInit = async ()=>{
+    try {
+        const myData= await pool.getConnection();
+        const rows = await myData.query("SELECT NOW ()");
+        console.log(rows[0])
+        await user();
+        await cart ();
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+export const query = async (text, params) => {
+    const start = Date.now()
+    const res = await pool.query(text, params)
+    const duration = Date.now() - start
+    console.log('executed query', { text, duration, rows: res.affectedRows })
+    return res
+  }
